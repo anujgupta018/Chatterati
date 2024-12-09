7f97430e8004adb5e4ab0f1d2d1f8ec34f2bf29e
Anuj <invincibleanuj1718@gmail.com>
2024-12-09 12:40:06 +0530

    Modified

--- Server/controllers/userControllers.js
+++ Server/controllers/userControllers.js
@@ -1,3 +1,28 @@
-module.exports.register = (req, res, next) => {
-    console.log(req.body)
+const User=require('../model/usersModel')
+const bcrypt=require('bcrypt')
+
+module.exports.register =async (req, res, next) => {    
+    // console.log(req.body)
+    try{
+        const {username,email,password}=req.body;
+    const userNameCheck=await User.findOne({username});
+    if(userNameCheck){
+        return res.json({msg:"Username already in use",status:false})
+    }
+    const emailCheck=await User.findOne({email});
+    if(emailCheck){
+        return res.json({msg:"Email already in use",status:false})
+    }
+    const hashedPassword=await bcrypt.hash(password,10);
+    const user=await User.create({
+        email,
+        username,
+        password:hashedPassword
+    })
+    delete user.password;
+    return res.json({status:true,user})
+    }
+    catch(error){
+        next(error);
+    }
 };
